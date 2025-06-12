5f961db3fecf0bcd45a3c29faf938451b35a5030
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-12 13:41:36 +0800

    ci: show generated hash

--- update.sh
+++ update.sh
@@ -3,11 +3,13 @@
 
 update_splayer_deps_hash() {
   hash_splayer="$(nix-build --impure --expr '(import ./pkgs {}).splayer.pnpmDeps.overrideAttrs (_: { outputHash = ""; outputHashAlgo = "sha256"; })' 2>&1 | tr -s ' ' | grep -Po "got: \K.+$")" || true
+  echo "SPlayer deps hash is: $hash_splayer"
   echo "\"$hash_splayer\"" >./pkgs/splayer/hash.nix
 }
 
 update_splayer_git_deps_hash() {
   hash_splayer_git="$(nix-build --impure --expr '(import ./pkgs {}).splayer-git.pnpmDeps.overrideAttrs (_: { outputHash = ""; outputHashAlgo = "sha256"; })' 2>&1 | tr -s ' ' | grep -Po "got: \K.+$")" || true
+  echo "SPlayer git deps hash is: $hash_splayer_git"
   echo "\"$hash_splayer_git\"" >./pkgs/splayer/hash-git.nix
 }
 
