43609301a5e873dd6d40b877bcba04f47d2e2866
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-15 00:39:07 +0800

    use easyOverlay and generate NUR packages from flake using provided overlay

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -1,17 +1,56 @@
-{ self, ... }:
+{ self, inputs, ... }:
 {
+  imports = [
+    inputs.flake-parts.flakeModules.easyOverlay
+  ];
   perSystem =
     { pkgs, ... }:
-    {
-      packages = import ./. { inherit pkgs; };
+    rec {
+      packages =
+        let
+          fetched-src = pkgs.callPackage ../_sources/generated.nix { };
+        in
+        rec {
+          danmakufactory = pkgs.callPackage ./danmakufactory {
+            sources = fetched-src.danmakufactory;
+          };
+          danmakufactory-git = pkgs.callPackage ./danmakufactory {
+            sources = fetched-src.danmakufactory-git;
+          };
+
+          mpv-handler = pkgs.callPackage ./mpv-handler { sources = fetched-src.mpv-handler; };
+
+          shijima-qt = pkgs.callPackage ./shijima-qt { };
+
+          splayer = pkgs.callPackage ./splayer {
+            pnpm = pkgs.pnpm_10;
+            hash = import ./splayer/hash.nix;
+            sources = fetched-src.splayer;
+          };
+          splayer-git = pkgs.callPackage ./splayer {
+            pnpm = pkgs.pnpm_10;
+            hash = import ./splayer/hash-git.nix;
+            sources = fetched-src.splayer-git;
+          };
+
+          uosc-danmaku = pkgs.mpvScripts.callPackage ./uosc-danmaku {
+            inherit danmakufactory;
+            sources = fetched-src.uosc-danmaku;
+          };
+          uosc-danmaku-git = pkgs.mpvScripts.callPackage ./uosc-danmaku {
+            danmakufactory = danmakufactory-git;
+            sources = fetched-src.uosc-danmaku-git;
+          };
+
+          wpsoffice-365 = pkgs.libsForQt5.callPackage ./wpsoffice-365 { };
+        };
+
+      overlayAttrs = packages;
     };
 
   flake = {
-    overlays.default = (_: pkgs: import ./. { inherit pkgs; });
-    nixosModules.default =
-      { ... }:
-      {
-        nixpkgs.overlays = [ self.overlays.default ];
-      };
+    nixosModules.default = {
+      nixpkgs.overlays = [ self.overlays.default ];
+    };
   };
 }
