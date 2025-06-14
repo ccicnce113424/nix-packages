ec6c3835645385f4b769366df6ed5ed5e8399565
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-15 01:04:39 +0800

    test: use flake-compat
    Hope that lazy-evaluation saves time and not causing timeout in NUR repo.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,4 @@
-# First, we use flake-compat in `../default.nix` if `getFlake` is not available.
-# Then, we use `toString` to convert a relative path into a string that contains an absolute path, so that `getFlake` is happy with it.
-# We also treat the overlay as a regular Nix function so that the provided nixpkgs is always being used.
 {
   pkgs ? import <nixpkgs> { },
 }:
-(builtins.getFlake or import (builtins.toString ../.)).overlays.default pkgs pkgs
+(import ../.).overlays.default pkgs pkgs
