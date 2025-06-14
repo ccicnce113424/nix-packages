e60911fc83c0688e7a22eb1bcbcccc28db72d4e3
ccicnce113424 <ccicnce113424@gmail.com>
2025-06-15 00:39:07 +0800

    use easyOverlay and generate NUR packages from flake using provided overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,40 +1,7 @@
+# First, we use flake-compat in `../default.nix` if `getFlake` is not available.
+# Then, we use `toString` to convert a relative path into a string that contains an absolute path, so that `getFlake` is happy with it.
+# We also treat the overlay as a regular Nix function so that the provided nixpkgs is always being used.
 {
   pkgs ? import <nixpkgs> { },
 }:
-let
-  fetched-src = pkgs.callPackage ../_sources/generated.nix { };
-in
-rec {
-  danmakufactory = pkgs.callPackage ./danmakufactory {
-    sources = fetched-src.danmakufactory;
-  };
-  danmakufactory-git = pkgs.callPackage ./danmakufactory {
-    sources = fetched-src.danmakufactory-git;
-  };
-
-  mpv-handler = pkgs.callPackage ./mpv-handler { sources = fetched-src.mpv-handler; };
-
-  shijima-qt = pkgs.callPackage ./shijima-qt { };
-
-  splayer = pkgs.callPackage ./splayer {
-    pnpm = pkgs.pnpm_10;
-    hash = import ./splayer/hash.nix;
-    sources = fetched-src.splayer;
-  };
-  splayer-git = pkgs.callPackage ./splayer {
-    pnpm = pkgs.pnpm_10;
-    hash = import ./splayer/hash-git.nix;
-    sources = fetched-src.splayer-git;
-  };
-
-  uosc-danmaku = pkgs.mpvScripts.callPackage ./uosc-danmaku {
-    inherit danmakufactory;
-    sources = fetched-src.uosc-danmaku;
-  };
-  uosc-danmaku-git = pkgs.mpvScripts.callPackage ./uosc-danmaku {
-    danmakufactory = danmakufactory-git;
-    sources = fetched-src.uosc-danmaku-git;
-  };
-
-  wpsoffice-365 = pkgs.libsForQt5.callPackage ./wpsoffice-365 { };
-}
+(builtins.getFlake or import (builtins.toString ../.)).overlays.default pkgs pkgs
