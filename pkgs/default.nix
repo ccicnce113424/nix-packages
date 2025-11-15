43fec906da78ce775781323a68f146bd2efeb6d3
ccicnce113424 <ccicnce113424@gmail.com>
2025-11-16 06:24:24 +0800

    fix splayer

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -70,7 +70,7 @@ rec {
   inherit (pkgs) splayer;
 
   splayer-git = pkgs.callPackage ./splayer-git rec {
-    inherit ((lib.importJSON ./splayer-git/hash.json)) hash;
+    inherit ((lib.importJSON ./splayer-git/src-info.json)) hash;
     sources = fetchedSrc.splayer-git;
     version = unstableVersion sources;
     inherit splayer;
